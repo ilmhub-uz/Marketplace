16a36e6781cca3c11e5dee48ab75c50495895574
Javlon Fattoev <javlonfattoev@gmail.com>
2023-06-13 19:53:03 +0500

    gateways

--- src/MarketPlace.Api/Program.cs
+++ src/MarketPlace.Api/Program.cs
@@ -1,3 +1,6 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", false, false);
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
@@ -21,5 +27,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
