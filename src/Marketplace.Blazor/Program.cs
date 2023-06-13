4655611e23c70835219872247c3655475ad41b07
Muhammadjon Mahkamboyev <muhammadjonmakhkamov1@gmail.com>
2023-06-13 09:34:55 -0700

    SignIn SignUp Profile

--- src/Marketplace.Blazor/Program.cs
+++ src/Marketplace.Blazor/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Marketplace.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -5,7 +6,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddBlazoredLocalStorageAsSingleton();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7075") });
 
 await builder.Build().RunAsync();
