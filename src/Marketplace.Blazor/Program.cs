66470186bc9ee56ed5edbfd604be68bf92a79103
Javlon Fattoev <javlonfattoev@gmail.com>
2023-06-13 20:10:55 +0500

    fronts

---
+++ src/Marketplace.Blazor/Program.cs
@@ -0,0 +1,11 @@
+using Marketplace.Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7075") });
+
+await builder.Build().RunAsync();
