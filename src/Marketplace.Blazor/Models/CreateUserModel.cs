cd04fd79f993484fe28e3fb0bf52657e81150093
ElbekLatifov <latifovelbek02@gmail.com>
2023-06-15 21:32:03 +0500

    bitta ikkita mayda chuyda form validation

--- src/Marketplace.Blazor/Models/CreateUserModel.cs
+++ src/Marketplace.Blazor/Models/CreateUserModel.cs
@@ -6,7 +6,8 @@ public class CreateUserModel
 {
 	public string Name { get; set; }
 	public string Password { get; set; }
-	[Compare(nameof(Password))]
+	[Compare("Password",
+		ErrorMessage = "Passwords must be same")]
 	public string ConfirmPassword { get; set; }
 	public string UserName { get; set; }
 }
