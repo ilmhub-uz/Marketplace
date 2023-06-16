8bf3c489ea25d39ed3e0acef865d4b3dfe4da4c9
Javlon Fattoev <javlonfattoev@gmail.com>
2023-06-16 21:15:34 +0500

    s

--- src/Marketplace.Organizations.Blazor/Managers/RequestManager.cs
+++ src/Marketplace.Organizations.Blazor/Managers/RequestManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using Blazored.LocalStorage;
 
 namespace Marketplace.Organizations.Blazor.Managers;
 
--- src/Marketplace.Services.Organizations/Program.cs
+++ src/Marketplace.Services.Organizations/Program.cs
@@ -11,7 +11,6 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
-});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
