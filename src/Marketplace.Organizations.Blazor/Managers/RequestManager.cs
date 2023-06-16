daf34c9a554d8bd16db7c2371203833ac6857bbb
Javlon Fattoev <javlonfattoev@gmail.com>
2023-06-16 21:12:03 +0500

    front

---
+++ src/Marketplace.Organizations.Blazor/Managers/RequestManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Json;
+
+namespace Marketplace.Organizations.Blazor.Managers;
+
+public class RequestManager
+{
+	private readonly ILocalStorageService _storage;
+	private readonly HttpClient _httpClient;
+
+	public RequestManager(ILocalStorageService storage, HttpClient httpClient)
+	{
+		_storage = storage;
+		_httpClient = httpClient;
+	}
+
+	public async Task<T?> Get<T>(string url) where T : class
+	{
+		var response = await SendAsync(url, HttpMethod.Get);
+		return await response.Content.ReadFromJsonAsync<T>();
+	}
+
+	public async Task<T?> Post<T>(string url, object body) where T : class
+	{
+		var content = JsonContent.Create(body);
+		var response = await SendAsync(url, HttpMethod.Post, content);
+		return await response.Content.ReadFromJsonAsync<T>();
+	}
+
+	public async Task<HttpResponseMessage> SendAsync(string url, HttpMethod method, HttpContent? content = null)
+	{
+		var token = await _storage.GetItemAsStringAsync("token");
+
+		var request = new HttpRequestMessage(method, url);
+		request.Headers.Add("Authorization", $"Bearer {token}");
+
+		if (content != null)
+			request.Content = content;
+
+		return await _httpClient.SendAsync(request);
+	}
+}
