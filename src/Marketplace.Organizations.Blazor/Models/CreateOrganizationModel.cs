64739b8090134a6c82fb19192aa1d24d5a110dee
Muhammadjon Mahkamboyev <muhammadjonmakhkamov1@gmail.com>
2023-06-16 02:41:59 -0700

    OrganizationUpdated Blazor pages were added

---
+++ src/Marketplace.Organizations.Blazor/Models/CreateOrganizationModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+
+namespace Marketplace.Organizations.Blazor.Models;
+
+public class CreateOrganizationModel
+{
+	public  string Name { get; set; }
+	public string? Description { get; set; }
+
+	public IBrowserFile? Logo { get; set; }
+	public string? Contact { get; set; }
+	public List<CreateAddressModel>? Addresses { get; set; }
+}
+
+public class CreateAddressModel
+{
+	public  string? Address { get; set; }
+}
